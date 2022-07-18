543983ac4a4b2dbd75f66d8a8ce7ab994ff2cb20
monicaa22 <iliescu.monica.e3m@student.ucv.ro>
2022-07-18 17:18:54 +0300

    first commit

---
+++ Entities/Entities/WeatherData.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class WeatherData : BaseModel
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public int Temperature  { get; set; }
+
+        public ICollection<UserAuthEntity> Users { get; set; }
+
+    }
+}
---
+++ Entities/Migrations/20220714183701_InitialMigration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weather",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Temperature = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weather", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAuths",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    KeyPassword = table.Column<byte[]>(type: "bytea", nullable: false),
+                    HashPassword = table.Column<byte[]>(type: "bytea", nullable: false),
+                    WeatherDataId = table.Column<int>(type: "integer", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAuths", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAuths_Weather_WeatherDataId",
+                        column: x => x.WeatherDataId,
+                        principalTable: "Weather",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAuths_WeatherDataId",
+                table: "UserAuths",
+                column: "WeatherDataId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAuths");
+
+            migrationBuilder.DropTable(
+                name: "Weather");
+        }
+    }
+}
